9dbc1dc800fc7a722b925fabc5c3ab6d7878c60a
Saku <saku.haikio@tuni.fi>
2025-03-03 12:22:56 +0200

    single consumer

--- atomic_queue.h
+++ atomic_queue.h
@@ -4,53 +4,6 @@
 
 constexpr int CACHE_LINE_SIZE = 64;
 
-class AB_lock_group
-{
-public:
-    void lock_A();
-    void lock_B();
-    void unlock_A();
-    void unlock_B();
-private:
-    std::atomic_int counter = 0;
-};
-
-void AB_lock_group::lock_A()
-{
-    int v = counter.load();
-    while(true)
-        if (v < 0)
-        {
-            v = counter.load();
-            continue;
-        }
-        else if(counter.compare_exchange_strong(v, v + 1))
-            break;
-}
-
-void AB_lock_group::lock_B()
-{
-    int v = counter.load();
-    while(true)
-        if (v > 0)
-        {
-            v = counter.load();
-            continue;
-        }
-        else if(counter.compare_exchange_strong(v, v - 1))
-            break;
-}
-
-void AB_lock_group::unlock_A()
-{
-    counter.fetch_sub(1);
-}
-
-void AB_lock_group::unlock_B()
-{
-    counter.fetch_add(1);
-}
-
 template<typename T>
 class atomic_queue
 {
@@ -92,8 +45,8 @@ public:
     alignas(CACHE_LINE_SIZE) container* buffer_;
     unsigned size_;
 
-    AB_lock_group A_lock;
-    AB_lock_group B_lock;
+    std::atomic_flag flag_A = ATOMIC_FLAG_INIT;
+    std::atomic_flag flag_B = ATOMIC_FLAG_INIT;
 
 };
 
@@ -104,7 +57,6 @@ atomic_queue<T>::atomic_queue(const unsigned size) :
     size_ = (size | 0xF) & 0xFFFF;
     for(unsigned i = 1; i <= 16; i *= 2)
         size_ |= size_ >> i;
-    //size_ = (1<<2) - 1;
 
     buffer_ = new container[size_ + 1];
 }
@@ -152,65 +104,29 @@ template<typename... Args>
 bool atomic_queue<T>::emplace(Args&&... args)
 {
     unsigned current_write = reserved_write_.load();
-    while(true)
-    {
-
-        A_lock.lock_A();
-        if(((current_write + 1) & size_) == read_.load())
-        {
-            A_lock.unlock_A();
-            return false;
-        }
-        auto b = reserved_write_.compare_exchange_strong(current_write, (current_write + 1) & size_);
-        A_lock.unlock_A();
-
-        if(b) break;
-    }
+    if(((current_write + 1) & size_) == read_.load())
+        return false;
+    reserved_write_.store((current_write + 1) & size_);
 
     buffer_[current_write].add_data(std::forward<Args>(args)...);
 
-    while(true)
-    {
-        unsigned t = current_write;
-
-        B_lock.lock_A();
-        auto b = write_.compare_exchange_strong(t, (current_write + 1) & size_);
-        B_lock.unlock_A();
-
-        if(b) return true;
-    }
+    write_.store((current_write + 1) & size_);
+    return true;
 }
 
 template<typename T>
 bool atomic_queue<T>::try_pop(T& value)
 {
     unsigned current_read = reserved_read_.load();
-    while(true)
-    {
-        B_lock.lock_B();
-        if(current_read == write_.load())
-        {
-            B_lock.unlock_B();
-            return false;
-        }
-        auto b = reserved_read_.compare_exchange_strong(current_read, (current_read + 1) & size_);
-        B_lock.unlock_B();
+    if(current_read == write_.load())
+        return false;
 
-        if(b) break;
-    }
+    reserved_read_.store((current_read + 1) & size_);
 
     container* can = buffer_ + current_read;
     value = std::move(can->move());
     can->destroy_data();
 
-    while(true)
-    {
-        unsigned t = current_read;
-
-        A_lock.lock_B();
-        auto b = read_.compare_exchange_strong(t, (current_read + 1) & size_);
-        A_lock.unlock_B();
-
-        if(b) return true;
-    }
+    read_.store((current_read + 1) & size_);
+    return true;
 }
