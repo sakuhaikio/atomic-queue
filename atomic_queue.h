fb4a42b3e1eded505d3409dd2d7bdbb585f2bf0a
Saku <saku@email>
2026-01-26 19:38:19 +0200

    add atomic bitfield

--- atomic_queue.h
+++ atomic_queue.h
@@ -1,10 +1,26 @@
 #include <atomic>
 #include <concepts>
+#include <memory>
 
 namespace atomic_ring
 {
 
 constexpr int CACHE_LINE_SIZE = 64;
+
+constexpr uint32_t lo32(uint64_t n) noexcept 
+{
+    return uint32_t(n); 
+}
+
+constexpr uint32_t hi32(uint64_t n) noexcept
+{
+    return uint32_t(n >> 32); 
+}
+
+constexpr uint64_t pack64(uint32_t lo, uint32_t hi) noexcept
+{
+    return (uint64_t(hi) << 32) | uint64_t(lo);
+}
 
 template<typename T>
 class atomic_queue
@@ -18,18 +34,12 @@ class atomic_queue
         constexpr void destroy_data();
         constexpr T&& move();
 
-        alignas(alignof(T)) unsigned char data[sizeof(T)];
-    };
-
-    struct S
-    {
-        unsigned a;
-        unsigned b;
+        alignas(alignof(T)) uint8_t data[sizeof(T)];
     };
 
 public:
 
-    atomic_queue(const unsigned size = 64);
+    atomic_queue(const uint32_t size = 64);
     ~atomic_queue() noexcept;
 
     atomic_queue() noexcept = delete;
@@ -45,33 +55,30 @@ private:
     template<typename... Args>
     bool emplace(Args&&... args);
 
-    alignas(CACHE_LINE_SIZE) std::atomic<S> rr_w_;
-    alignas(CACHE_LINE_SIZE) std::atomic<S> rw_r_;
+    alignas(CACHE_LINE_SIZE) std::atomic<uint64_t> rr_w_bits;
+    alignas(CACHE_LINE_SIZE) std::atomic<uint64_t> rw_r_bits;
 
-    alignas(CACHE_LINE_SIZE) container* buffer_;
-
-    unsigned size_;
+    alignas(CACHE_LINE_SIZE) std::unique_ptr<container[]> buffer_;
+    uint32_t size_;
 };
 
 template<typename T>
 atomic_queue<T>::atomic_queue(const unsigned size) :
-    rr_w_({0, 0}), rw_r_({0, 0})
+    rw_r_bits(0), rr_w_bits(0)
 {
     size_ = (size | 0xF) & 0xFFFF;
     for(unsigned i = 1; i <= 16; i *= 2)
         size_ |= size_ >> i;
 
-    buffer_ = new container[size_ + 1];
+    buffer_ = std::make_unique<container[]>(size_ + 1);
 }
 
 template<typename T>
 atomic_queue<T>::~atomic_queue() noexcept
 {
-    while(rr_w_.load(std::memory_order_relaxed).b != rw_r_.load(std::memory_order_relaxed).b)
-        buffer_[rw_r_.exchange({rw_r_.load(std::memory_order_relaxed).a, (rw_r_.load(std::memory_order_relaxed).b + 1) & size_}, 
-            std::memory_order_relaxed).b].destroy_data();
-        
-    delete[] buffer_;
+//    while(rr_w_.load(std::memory_order_relaxed).b != rw_r_.load(std::memory_order_relaxed).b)
+//        buffer_[rw_r_.exchange({rw_r_.load(std::memory_order_relaxed).a, (rw_r_.load(std::memory_order_relaxed).b + 1) & size_}, 
+//            std::memory_order_relaxed).b].destroy_data();
 }
 
 template<typename T> atomic_queue<T>::container::container()
@@ -87,7 +94,7 @@ template<typename T>
 template<typename... Args>
 constexpr void atomic_queue<T>::container::add_data(Args&&... args)
 {
-    new(&data) T(std::forward<Args>(args)...);
+    ::new(&data) T(std::forward<Args>(args)...);
 }
 
 template<typename T>
@@ -106,23 +113,33 @@ template<typename T>
 template<typename... Args>
 bool atomic_queue<T>::emplace(Args&&... args)
 {
-    S curr_rw_r = rw_r_.load(std::memory_order_relaxed);
+    uint64_t curr_rw_r = rw_r_bits.load(std::memory_order_relaxed);
+    uint32_t rw; uint32_t r;
     do
-        if(((curr_rw_r.a + 1) & size_) == curr_rw_r.b)
+    {
+        rw = lo32(curr_rw_r);
+        r = hi32(curr_rw_r);
+        if(((rw + 1) & size_) == r)
             return false;
-    while(! rw_r_.compare_exchange_strong(
+    }
+    while(! rw_r_bits.compare_exchange_strong(
         curr_rw_r, 
-        {(curr_rw_r.a + 1) & size_, curr_rw_r.b},
+        pack64(rw + 1 & size_, r),
         std::memory_order_acquire,
         std::memory_order_relaxed));
 
-    buffer_[curr_rw_r.a].add_data(std::forward<Args>(args)...);
+    buffer_[rw].add_data(std::forward<Args>(args)...);
 
-    S t = rr_w_.load(std::memory_order_relaxed);
+    uint64_t curr_rr_w = rr_w_bits.load(std::memory_order_relaxed);
+    uint32_t rr; uint32_t w;
     do 
-        t.b = curr_rw_r.a;
-    while(!rr_w_.compare_exchange_strong(
-        t, {t.a, (t.b + 1) & size_},
+    {
+        rr = lo32(curr_rr_w);
+        w = rw;
+    }
+    while(!rr_w_bits.compare_exchange_strong(
+        curr_rr_w, 
+        pack64(rr, (w + 1) & size_),
         std::memory_order_release,
         std::memory_order_relaxed));
 
@@ -132,25 +149,33 @@ bool atomic_queue<T>::emplace(Args&&... args)
 template<typename T>
 bool atomic_queue<T>::try_pop(T& value)
 {
-    S curr_rr_w = rr_w_.load(std::memory_order_relaxed);
+    uint64_t curr_rr_w = rr_w_bits.load(std::memory_order_relaxed);
+    uint32_t rr; uint32_t w;
     do
-        if(curr_rr_w.a == curr_rr_w.b)
+    {
+        rr = lo32(curr_rr_w);
+        w = hi32(curr_rr_w);
+        if(rr == w)
             return false;
-    while(! rr_w_.compare_exchange_strong(
-        curr_rr_w, 
-        {(curr_rr_w.a + 1) & size_, curr_rr_w.b},
+    }
+    while(! rr_w_bits.compare_exchange_strong(
+        curr_rr_w, pack64((rr + 1) & size_, w),
         std::memory_order_acquire,
         std::memory_order_relaxed));
 
-    container* can = buffer_ + curr_rr_w.a;
+    container* can = buffer_.get() + rr;
     value = std::move(can->move());
     can->destroy_data();
 
-    S t = rw_r_.load(std::memory_order_relaxed);
+    uint64_t curr_rw_r = rw_r_bits.load(std::memory_order_relaxed);
+    uint32_t rw; uint32_t r;
     do 
-        t.b = curr_rr_w.a;
-    while(!rw_r_.compare_exchange_strong(
-        t, {t.a, (t.b + 1) & size_},
+    {
+        rw = lo32(curr_rw_r);
+        r = rr;
+    }
+    while(!rw_r_bits.compare_exchange_strong(
+        curr_rw_r, pack64(rw, (r + 1) & size_),
         std::memory_order_release,
         std::memory_order_relaxed));
 
